31c7f672f6ee79fcb162871ac61dbfa648dee5e4
hrmorley34 <henry@morley.org.uk>
2020-11-13 18:16:15 +0000

    Add utility updater

---
+++ bash/update-utils.sh
@@ -0,0 +1,10 @@
+# Go to folder containing this file
+cd "${0%/*}"
+
+# Only try to fast-forward pull; otherwise prompt user
+if git pull --ff-only; then
+  : # Success!
+else
+  echo "Failed fast-forward; merge manually" 1>&2
+  exit 1
+fi
